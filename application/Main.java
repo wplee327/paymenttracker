f80e4b34e0f76266733e836926ae86cc02d9343c
Warren Lee <wplee.327@gmail.com>
2016-12-09 21:23:31 -0500

    Initial Commit. GUI built in fxml using Scene Builder 2.0 and manual CSS.

---
+++ application/Main.java
@@ -0,0 +1,28 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = FXMLLoader.load(Main.class.getClassLoader().getResource("Root.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.setMinWidth(1047);
+			primaryStage.setMinHeight(553);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
