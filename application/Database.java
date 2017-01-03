49051ae3e8d8eb7ee80a92947836becd56595789
Warren Lee <wplee.327@gmail.com>
2017-01-02 19:16:53 -0500

    Added Clients class to contain query data. Added getAllClients() to return a List<Clients>. Modified sqlStrings to deal with primary key correctly. Moved .close() for statements to end of try{} to avoid closed ResultSet.

--- application/Database.java
+++ application/Database.java
@@ -5,9 +5,67 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
+
+import application.Database.Clients;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 public class Database {
 	private Connection dbConnection = null;
+
+	public class Clients {
+		private IntegerProperty key;
+		private StringProperty lastName, firstName;
+
+		public void setKey(int key) {
+			KeyProperty().set(key);
+		}
+
+		public int getKey() {
+			return KeyProperty().get();
+		}
+
+		public IntegerProperty KeyProperty() {
+			if (key == null) {
+				key = new SimpleIntegerProperty(this, "key");
+			}
+			return key;
+		}
+
+		public void setLastName(String lastName) {
+			LastNameProperty().set(lastName);
+		}
+
+		public String getLastName() {
+			return LastNameProperty().get();
+		}
+
+		public StringProperty LastNameProperty() {
+			if (lastName == null) {
+				lastName = new SimpleStringProperty(this, "lastName");
+			}
+			return lastName;
+		}
+
+		public void setFirstName(String firstName) {
+			FirstNameProperty().set(firstName);
+		}
+
+		public String getFirstName() {
+			return FirstNameProperty().get();
+		}
+
+		public StringProperty FirstNameProperty() {
+			if (firstName == null) {
+				firstName = new SimpleStringProperty(this, "firstName");
+			}
+			return firstName;
+		}
+	}
 
 	public Database(String dbName) {
 		if (existsDatabase(dbName)) {
@@ -45,14 +103,43 @@ public class Database {
 		}
 	}
 
+	public List<Clients> getAllClients() {
+		Statement sqlStatement = null;
+		List<Clients> clientsList = new LinkedList<Clients>();
+		try {
+			sqlStatement = dbConnection.createStatement();
+			String sqlString = "SELECT * FROM CLIENTS";
+			ResultSet tempResult = sqlStatement.executeQuery(sqlString);
+			while (tempResult.next()) {
+				int key = tempResult.getInt(1);
+				String lastName = tempResult.getString(2);
+				String firstName = tempResult.getString(3);
+				Clients client = new Clients();
+				client.setKey(key);
+				client.setLastName(lastName);
+				client.setFirstName(firstName);
+				clientsList.add(client);
+			}
+			sqlStatement.close();
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+		return clientsList;
+	}
+
 	public void addClient(String lastName, String firstName) {
 		Statement sqlStatement = null;
 		int result = Statement.EXECUTE_FAILED;
 		try {
 			sqlStatement = dbConnection.createStatement();
-			String sqlString = "INSERT INTO CLIENTS (LASTNAME, FIRSTNAME) VALUES ('" + lastName + "', '" + firstName
-					+ "')";
-			result = sqlStatement.executeUpdate(sqlString);
+			String sqlString = "SELECT * FROM CLIENTS WHERE LASTNAME = '" + lastName + "' AND FIRSTNAME = '" + firstName
+					+ "'";
+			ResultSet tempResult = sqlStatement.executeQuery(sqlString);
+			if (!tempResult.next()) {
+				sqlString = "INSERT INTO CLIENTS (ID, LASTNAME, FIRSTNAME) VALUES (NULL, '" + lastName + "', '"
+						+ firstName + "')";
+				result = sqlStatement.executeUpdate(sqlString);
+			}
 			sqlStatement.close();
 		} catch (Exception exception) {
 			exception.printStackTrace();
@@ -82,8 +169,8 @@ public class Database {
 			String sqlString = "SELECT ID FROM CLIENTS WHERE LASTNAME = '" + lastName + "' AND FIRSTNAME = '"
 					+ firstName + "'";
 			ResultSet result = sqlStatement.executeQuery(sqlString);
-			sqlStatement.close();
 			idNum = result.getInt("ID");
+			sqlStatement.close();
 		} catch (Exception exception) {
 			exception.printStackTrace();
 		}
@@ -98,10 +185,10 @@ public class Database {
 			String sqlString = "SELECT UNPAID, PENDING, PAID FROM PAYMENTS WHERE CLIENTID = "
 					+ Integer.toString(clientID) + " AND DATE = " + date;
 			ResultSet result = sqlStatement.executeQuery(sqlString);
-			sqlStatement.close();
 			for (int idx = 0; idx < 3; idx++) {
 				paymentsArray[idx] = result.getInt(idx + 1);
 			}
+			sqlStatement.close();
 		} catch (Exception exception) {
 			exception.printStackTrace();
 		}
@@ -112,12 +199,12 @@ public class Database {
 		Statement sqlStatement = null;
 		try {
 			sqlStatement = dbConnection.createStatement();
-			String sqlString = "CREATE TABLE CLIENTS " + "(ID INT PRIMARY KEY NOT NULL, " + "LASTNAME TEXT NOT NULL, "
+			String sqlString = "CREATE TABLE CLIENTS " + "(ID INTEGER PRIMARY KEY, " + "LASTNAME TEXT NOT NULL, "
 					+ "FIRSTNAME TEXT NOT NULL)";
 			sqlStatement.executeUpdate(sqlString);
-			sqlString = "CREATE TABLE PAYMENTS " + "(ID INT PRIMARY KEY NOT NULL, " + "CLIENTID INT NOT NULL, "
-					+ "DATE TEXT NOT NULL, " + "UNPAID INT NOT NULL, " + "PENDING INT NOT NULL, "
-					+ "PAID INT NOT NULL)";
+			sqlString = "CREATE TABLE PAYMENTS " + "(ID INTEGER PRIMARY KEY, " + "CLIENTID INTEGER NOT NULL, "
+					+ "DATE TEXT NOT NULL, " + "UNPAID INTEGER NOT NULL, " + "PENDING INTEGER NOT NULL, "
+					+ "PAID INTEGER NOT NULL)";
 			sqlStatement.executeUpdate(sqlString);
 			sqlStatement.close();
 		} catch (Exception exception) {
